fb3816e879731dcf3954f6636867d45c993efdd0
zjx6057 <zjx6057@gmail.com>
2025-12-23 14:47:05 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  },
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY || "")
   },
