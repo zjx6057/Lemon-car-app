708222b10c85c25f904b3c3eff41704a5e03dd36
zjx6057 <zjx6057@gmail.com>
2025-12-23 14:10:12 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    // 注入 API_KEY，确保 Gemini 服务在生产环境不会因为 process.env 未定义而崩溃
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY || "")
   },
   build: {
